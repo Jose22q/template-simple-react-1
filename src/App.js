232e4f1f34f85889f252bab7d8c24e8b2ddd7f21
Jose22q <104874475+Jose22q@users.noreply.github.com>
2022-07-05 17:09:35 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Navbar from "./componentes/Navbar";
 import Carrusel from "./componentes/Carrusel";
 import Card from "./componentes/Card";
